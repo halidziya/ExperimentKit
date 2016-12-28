ed48213b480ce34a223197e5097fcf3a4b88af19
Halid Yerebakan <hzyereba@tesla.cs.iupui.edu>
2016-12-27 22:20:59 -0500

    Working

--- i2s_flowcap3.m
+++ i2s_flowcap3.m
@@ -29,7 +29,7 @@ X=igmm_normalize(X,20,true);
     burn_in='1600';
     step='10';
     fprintf(1,'I2GMM is running...\n');
-    cmd = ['i2slice ',data,' ',meanpath,' ',psipath,' ',params,' ',num_sweeps,' ', burn_in,' ',step];
+    cmd = ['./i2slice ',data,' ',meanpath,' ',psipath,' ',params,' ',num_sweeps,' ', burn_in,' ',step];
     tic;
     system(cmd);
     elapsed = toc;
